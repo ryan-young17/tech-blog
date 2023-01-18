470a8fcc85d61998eceef8c0d341f18332fd87db
Ryan Young <ryanbyoung1@gmail.com>
2023-01-18 17:47:17 -0500

    set up server.js, added sync.js to db folder, added routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const {} = require('../models/');
+
+router.get('/', async (req, res) => {
+  try {
+    res.render('homepage');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+});
+
+module.exports = router;
