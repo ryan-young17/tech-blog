314160fdaa56895d63e918d12fcc03f4a7d7e90a
Ryan Young <ryanbyoung1@gmail.com>
2023-01-25 14:41:01 -0500

    added post and update routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -39,6 +39,41 @@ router.get('/dashboard', async (req, res) => {
   }
 });
 
+router.post('/dashboard', async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      title: req.body.title,
+      content: req.body.content,
+      user_id: req.session.user_id
+    });
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put('/dashboard', async (req, res) => {
+  try {
+    const updatedPost =  await Post.update({
+      title: req.body.title,
+      content: req.body.content
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!updatedPost) {
+      res.status(404).json({ message: 'No post was found with that id' });
+      return;
+    }
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.post('/', async (req, res) => {
   try {
     const newUser = await User.create({
