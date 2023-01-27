5e4449540103d44f836127f710ec3a44db8a874d
Ryan Young <ryanbyoung1@gmail.com>
2023-01-27 17:14:43 -0500

    commented out references

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -47,15 +47,15 @@ router.get('/post/:id', async (req, res) => {
         {
           model: Comment,
           attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
+          // include: {
+          //   model: User,
+          //   attributes: ['username']
+          // }
         },
-        {
-          model: User,
-          attributes: ['username']
-        }
+        // {
+        //   model: User,
+        //   attributes: ['username']
+        // }
       ],
       where: {
         id: req.params.id
